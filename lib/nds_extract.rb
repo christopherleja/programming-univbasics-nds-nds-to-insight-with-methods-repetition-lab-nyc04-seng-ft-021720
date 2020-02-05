92149b0c71c1b5d19d2139b6db5135bca5def0d4
Christopher Leja <christopher.leja@gmail.com>
2020-02-05 21:41:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -38,9 +38,7 @@ def total_gross(source)
   row_index = 0
   combined_gross = 0
   director_name = source[row_index][:name]
-  #dir_data = source[row_index]
- # source_name = d
-#puts name_in_source
+
   while row_index < source.length do
     
       combined_gross += gross_for_director(director_name)
