55fd584292075d6703b42a16c876edc5dae4bca1
Christopher Leja <christopher.leja@gmail.com>
2020-02-05 22:39:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -38,7 +38,7 @@ def total_gross(source)
   row_index = 0
   combined_gross = 0
   puts source[row_index][:movies][5][:worldwide_gross]
-  puts source[row_index][:movies].class
+  puts source[row_index][:names].class
     while row_index < source.length do
     director_name = source[row_index][:name]
     puts director_name
